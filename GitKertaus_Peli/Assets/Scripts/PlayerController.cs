a867b677d6a08c53cdae95de14002f23446e6f65
Olli <olli.juhola@careeria.fi>
2021-03-11 10:37:44 +0200

    Simple Player Movement Added

---
+++ GitKertaus_Peli/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(moveDir * Time.deltaTime * speed);
+    }
+}
